42956e46e11502733f6587f71da03a63ad000595
Pablo Lefort <lefortpablo@gmail.com>
2019-11-17 20:44:38 -0300

    WIP

--- tpo.py
+++ tpo.py
@@ -1,92 +1,67 @@
-import copy
-
-CITIES = ['BS AS', 'La Plata', 'Corrientes', 'Cordoba']
+CITIES = ['BS AS', 'La Plata', 'Corrientes', 'Cordoba', 'Salta', 'Puerto Madryn', 'Neuquen', 'Cachari']
 DISTANCES = [
-    [90, 100, 140],
-    [0, 70, 80],
-    [70, 0, 90],
-    [80, 90, 0]
+    [90, 100, 140, 500, 1, 100, 250],
+    [0, 70, 80, 90, 3, 20, 270],
+    [70, 0, 90, 100, 7, 10, 600],
+    [80, 90, 0, 3, 100, 80, 900],
+    [90, 100, 3, 0, 70, 80, 1000],
+    [3, 7, 100, 70, 0, 500, 1500],
+    [20, 10, 80, 80, 500, 0, 3],
+    [270, 600, 900, 1000, 1500, 3, 0]
 ]
 
 
-CITIES = ['BS AS', 'La Plata']
+def alg_two():
+    min_cost = 0
+    result = ''
+    partials = {}
 
-
-'''
-complejidad minima!!! 2^(n-1) - 1
-- cantidad de ciudades / 2 -> recorrido para cada vector: 5 ciudades = 2.5 => int(2.5+1) = 3. MEDIO
-- recorrer la matriz armando las combinaciones. Siendo para el primero c[i->MEDIO+j] = A, c[MEDIO->n-1] = B. j=-1
-- i+1. En B agregar en orden. j si se pasa de n => j=n
-- 
-
-
-- recorrer ciudades hasta completar los dos caminos
-- obtener siguiente ciudad
-- agregar a ambos caminos siendo 3 o 4 combinaciones posibles
-- calcular costo de las combinaciones
-- actualizar caminos con la mejor combinacion
-'''
-
-def distance(paths):
-    paths_length = len(paths)
-    if paths_length == 0 or paths_length == 1:
-        return 0
-    else:
-        aux = 0
-        size = len(paths)
-        for i in range(size):
-            if (i+1) < size:
-                aux += DISTANCES[paths[i]][paths[i+1]-1]
-        return aux
-
-
-def alg():
-
-    A, B = [], []
-    aux_A, aux_B = [], []
     for i in range(len(CITIES)):
-        #print(i)
-        #next_city = CITIES.pop(0)
-        CITIES.pop(0)
+        combinations = 2**(i+1) - 2
+        for j in range(combinations):  # range goes from zero to last number less one
+            aux = bin(j+1)[2:].zfill(i+1)
 
-        # add to A
-        aux_A = copy.deepcopy(A)
-        aux_A.append(i)
+            # stop iteration when found an already calculated result
+            if partials.get(aux, False):
+                break
 
-        # add to B
-        aux_B = copy.deepcopy(B)
-        aux_B.append(i)
+            new_city = len(aux) - 2
+            try:
+                partial = partials[aux[1:]]
+                a_or_b = aux[0]
+                cost = 0
+                if a_or_b == aux[1]:
+                    cost = DISTANCES[len(aux)-2][new_city]
+                else:
+                    found = False
+                    k = 2
+                    while not found:
+                        if a_or_b == aux[k]:
+                            found = True
+                        else:
+                            k += 1
 
-        if len(aux_A) == 1:
-            A = aux_A
-            continue
-        elif distance(aux_A) + distance(B) < distance(A) + distance(aux_B):
-            A = aux_A
-        else:
-            B = aux_B
+                    cost = DISTANCES[len(aux)-(k+1)][new_city]
 
-        # take last from A, send to B, add to A
-    
-    print(A)
-    print(B)
+                partials[aux] = partial + cost
+            except KeyError:
+                if i > 1:
+                    # remove first and last
+                    partial = partials[aux[1:-1]+aux[0]]
+                    # only add the first city to the second one
+                    partials[aux] = partial + DISTANCES[0][1]
+                else:
+                    partials[aux] = 0
+
+            # Avoid recalculation for same paths
+            partials[bin(combinations-j)[2:].zfill(i+1)] = partials[aux]
+
+            if partials[aux] < min_cost or j == 0:
+                result = aux
+                min_cost = partials[aux]
 
 
-#alg()
+    print(result)
+    print(min_cost)
 
-
-
-
-def combineta():
-    A = []
-    B = []
-
-    a = 1
-    b = len(CITIES)
-
-    for i in range(b):
-        if i >= 1:
-            B = CITIES[:i]
-
-        B += CITIES[i+1:]
-        print(f'A = [{CITIES[i]}], B = [{B}]')
-        B = []
+alg_two()
